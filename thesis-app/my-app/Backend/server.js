702fcfcba320b3e849d21732ba4456e8a2478dd5
AuraKnight04 <lrafaelvazquez@gmail.com>
2025-02-27 00:23:46 -0500

    Connect to db with node js and aws and sql

--- thesis-app/my-app/Backend/server.js
+++ thesis-app/my-app/Backend/server.js
@@ -3,10 +3,23 @@ const app = express();
 const port = 3001;
 const mysql = require('mysql');
 
+
+// npmjs.com reference 
+
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'db-thesis-app.cjqoma42a22a.us-east-2.rds.amazonaws.com',
     user: 'admin',
     password: '49311$%^Pr',
-    database: 'thesis'
+    database: 'Gamified_ToDoList_DB'
 });
+
+db.connect(function(err) {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database with threadId: ' + db.threadId);
+});
+
+
 
